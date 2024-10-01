db23db165594726aff1de392ab3db1c57ad5f004
pabllo lima <stevaopabllo@gmai.com>
2024-10-01 13:19:31 -0300

    mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+import org.w3c.dom.ls.LSOutput;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    Curso curso1 = new Curso();
+    curso1.setTitulo("Curso java");
+    curso1.setDescricao("descricao curso java");
+    curso1.setCargaHoraria(8);
+
+
+    Curso curso2 = new Curso();
+    curso2.setTitulo("Curso jS");
+    curso2.setDescricao("descricao curso jS");
+    curso2.setCargaHoraria(4);
+
+
+    System.out.println(curso1);
+    System.out.println(curso2);
+
+    Mentoria mentoria = new Mentoria();
+    mentoria.setTitulo("mentoria java");
+    mentoria.setDescricao("descricao mentoria java");
+    mentoria.setData(LocalDate.now());
+
+    System.out.println(mentoria);
     }
 }
---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,48 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+
+    public Curso() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    private  String titulo;
+private  String descricao;
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    private  int cargaHoraria;
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
+
